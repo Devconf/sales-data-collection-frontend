8d0e923ffbbe4f0b21db2acb2e8e100ed5c6c2cd
kim hyun sik <devconf5296@gmail.com>
2022-01-19 19:22:07 +0900

    Add Style lib

---
+++ src/lib/styles/globalStyle.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+	${normalize}
+	html,
+	body { 
+		min-width : 850px;
+		height: 100%;
+    padding : 0;
+    margin : 0 auto;
+    box-sizing: border-box;
+    font-family: Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, 'Helvetica Neue', 'Segoe UI', 'Apple SD Gothic Neo', 'Noto Sans KR', 'Malgun Gothic', sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+		color: ${({ theme }) => theme.colors.main}
+	}
+	#root {
+		height: 100%;
+	}
+	* {
+		box-sizing: border-box;
+	}
+`;
+
+export default GlobalStyle;
---
+++ src/lib/styles/styled.d.ts
@@ -0,0 +1,35 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    fontSizes: {
+      header1: string;
+      header2: string;
+      header3: string;
+      body: string;
+    };
+
+    colors: {
+      bold: string;
+      main: string;
+      sub: string;
+      placeholder: string;
+      white: string;
+      background: string;
+      bgDivider: string;
+      disabledbg: string;
+      disabledborder: string;
+      scrollBar: string;
+      button_color1: string;
+      button_color2: string;
+      primary60: string;
+      primary40: string;
+      primary20: string;
+      secondary100: string;
+      secondary80: string;
+      danger1: string;
+      danger2: string;
+      warning: string;
+    };
+  }
+}
