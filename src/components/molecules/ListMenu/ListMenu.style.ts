0506eedbfb38ac0608092e0bdf3611908dbd6d1f
kim hyun sik <devconf5296@gmail.com>
2022-01-26 21:20:11 +0900

    Feat:Create ListMenu component

---
+++ src/components/molecules/ListMenu/ListMenu.style.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Wrapper =styled.div`
+    width :246px;
+    height : 60px;
+`
+
+export const StyledLink = styled(Link)`
+    text-decoration-line : none;
+    font-size: 15pt;
+    color: black;
+    text-align: center;
+    position: relative;
+    top: 30%;
+`
