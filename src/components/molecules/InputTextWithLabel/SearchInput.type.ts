613131adace9609381ab00c5b78647045483a344
kim hyun sik <devconf5296@gmail.com>
2022-01-26 15:53:38 +0900

    Feat:Change InputTextWithLabel component name

--- src/components/molecules/InputTextWithLabel/SearchInput.type.ts
+++
@@ -1,5 +0,0 @@
-import {InputProps} from "@components/atoms/Input/Input.type";
-
-export interface SearchInputProps extends InputProps {
-    label: string;
-}
---
+++ src/components/molecules/InputWithLabel/InputWithLabel.type.ts
@@ -0,0 +1,5 @@
+import {InputProps} from '@components/atoms/Input/Input.type';
+
+export interface InputWithLabelProps extends InputProps {
+    label: string;
+}
